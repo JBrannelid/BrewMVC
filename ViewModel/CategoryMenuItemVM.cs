354d2e30f72d35378ad44423c0ddfa574163bbf4
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-02 12:05:20 +0200

    Add CategoryMenuItemVM to support grouping of menu items by category.

---
+++ ViewModel/CategoryMenuItemVM.cs
@@ -0,0 +1,10 @@
+﻿using BrewMVC.Models;
+
+namespace BrewMVC.ViewModel
+{
+    public class CategoryMenuItemVM
+    {
+        public Dictionary<string, List<MenuItems> >MenuByCategory { get; set; }
+        public List<string> CategoryOrder { get; set; } = new() { "Salads", "Bowls", "Beverages", "Desserts" };
+    }
+}
