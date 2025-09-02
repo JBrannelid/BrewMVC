35bd9adc9e9754b6bd7a7264d7db280c0913b69c
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-02 12:04:51 +0200

    Remove unused Name property from PopularMenuItemVM to simplify model.

--- ViewModel/PopularMenuItemVM.cs
+++ ViewModel/PopularMenuItemVM.cs
@@ -5,7 +5,6 @@ namespace BrewMVC.ViewModels
     public class PopularMenuItemVM
     {
         public string? Description { get; set; }
-        public string Name { get; set; }
         public string ImageUrl { get; set; }
     }
 }
