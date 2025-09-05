310c564e21ed47a68633bde76d5855d0dcc63ff0
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 19:34:16 +0200

    Initialize HttpClient injection in BookingController for API communication

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -4,6 +4,13 @@ namespace BrewMVC.Controllers
 {
     public class BookingController : Controller
     {
+        private readonly HttpClient _client;
+
+        public BookingController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("BrewAPI");
+        }
+
         public IActionResult Index()
         {
             return View();
