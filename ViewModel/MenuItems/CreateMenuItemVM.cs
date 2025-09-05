b85ca5237b2f6fd5ef1dc78cd80cd40ffbb4f279
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 21:39:37 +0200

    Add CreateMenuItemVM for menu item creation operations

---
+++ ViewModel/MenuItems/CreateMenuItemVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrewMVC.ViewModel.MenuItems
+{
+    public class CreateMenuItemVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [StringLength(maximumLength: 30, ErrorMessage = "Category cannot exceed 30 characters")]
+        public string? Category { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 9999.99, ErrorMessage = "Price must be between range of: 0.01 - 9999.99 kr")]
+        public decimal Price { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Is Popular")]
+        public bool IsPopular { get; set; } = false;
+
+        [Display(Name = "Image URL")]
+        [Url(ErrorMessage = "ImageUrl must be a valid URL-adress")]
+        public string? ImageUrl { get; set; }
+    }
+}
