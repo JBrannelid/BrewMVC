1f31d15aec235eac29504bd8b106836ac6e0aef2
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 19:34:17 +0200

    Add admin booking controller with authorization and API integration

---
+++ Controllers/AdminBookingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace BrewMVC.Controllers
+{
+    [Authorize]
+    public class AdminBookingController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public AdminBookingController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("BrewAPI");
+        }
+
+        private void SetAuthorizationHeader()
+        {
+            var token = HttpContext.Request.Cookies["jwtToken"];
+            if (!string.IsNullOrEmpty(token))
+            {
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
+
+        public IActionResult Index()
+        {
+            SetAuthorizationHeader();
+
+            return View();
+        }
+    }
+}
