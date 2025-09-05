275e50615dbd5d6438f042737d4ea7a090fd6aea
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-05 19:34:16 +0200

    Add new model classes with improved naming and structure

---
+++ Models/MenuItemModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BrewMVC.Models
+{
+    public class MenuItemModel
+    {
+        public int MenuItemId { get; set; }
+        public string Name { get; set; }
+        public string? Category { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public bool IsPopular { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
---
+++ Models/TokenResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BrewMVC.Models
+{
+    public class TokenResponseModel
+    {
+        public string Token { get; set; }
+    }
+}
