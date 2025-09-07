32e4dae07b5a3665c166bda5600d101cf14ece28
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-07 20:16:02 +0200

    Add CustomerModel for MVC customer data representation

---
+++ Models/CustomerModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BrewMVC.Models
+{
+    public class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string? Email { get; set; }
+    }
+}
