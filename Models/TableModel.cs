c8010d8f4794a36975019787491d3b9bb7e04022
Johannes Brannelid <83164833+JBrannelid@users.noreply.github.com>
2025-09-07 20:16:02 +0200

    Add TableModel for MVC table data representation

---
+++ Models/TableModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BrewMVC.Models
+{
+    public class TableModel
+    {
+        public int TableId { get; set; }
+        public int TableNumber { get; set; }
+        public int Capacity { get; set; }
+    }
+}
